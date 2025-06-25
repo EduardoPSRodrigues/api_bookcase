bb834e4f11768733c9a0c794b2e49b4d78729f87
EduardoPSRodrigues <eduardopsrodrigues.dev@gmail.com>
2025-06-25 16:55:46 -0300

    Criando dbConnect com Mongo Atlas

---
+++ src/config/dbConnect.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+async function conectarBancoDeDados() {
+  
+    await mongoose.connect(process.env.DB_CONNECTION_STRING);
+    
+    console.log('Conexão com o MongoDB Atlas estabelecida com sucesso!');
+    return mongoose.connection;
+
+}
+
+export default conectarBancoDeDados;
