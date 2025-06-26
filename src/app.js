c7c3766e829404c22e32c2922ddec04af3255611
EduardoPSRodrigues <eduardopsrodrigues.dev@gmail.com>
2025-06-26 10:55:00 -0300

    Refact rotas no app.js

--- src/app.js
+++ src/app.js
@@ -15,49 +15,4 @@ conexao.on("error", (error) => {
 const app = express();
 routes(app); // Configurar as rotas
 
-// app.get("/livros/:id", async (req, res) => {
-//   const id = req.params.id;
-//   const livroEncontrado = await livro.findById(id);
-
-//   if (livro) {
-//     res.status(200).json(livro);
-//   } else {
-//     res.status(404).send("Livro não encontrado");
-//   }
-// });
-
-// app.post("/livros", (req, res) => {
-//   const novoLivro = {
-//     id: livros.length + 1,
-//     titulo: req.body.titulo,
-//   };
-
-//   livros.push(novoLivro);
-//   res.status(201).json(novoLivro);
-// });
-
-// app.put("/livros/:id", (req, res) => {
-//   const id = parseInt(req.params.id, 10);
-//   const livro = livros.find((l) => l.id === id);
-
-//   if (livro) {
-//     livro.titulo = req.body.titulo;
-//     res.status(200).json(livro);
-//   } else {
-//     res.status(404).send("Livro não encontrado");
-//   }
-// });
-
-// app.delete("/livros/:id", (req, res) => {
-//   const id = parseInt(req.params.id, 10);
-//   const index = livros.findIndex((l) => l.id === id);
-
-//   if (index !== -1) {
-//     livros.splice(index, 1);
-//     res.status(204).send();
-//   } else {
-//     res.status(404).send("Livro não encontrado");
-//   }
-// });
-
 export default app;
