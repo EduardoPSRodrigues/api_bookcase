22edbc7a115cf26e62af30194f0cb4e7875c223b
EduardoPSRodrigues <eduardopsrodrigues.dev@gmail.com>
2025-06-26 11:43:43 -0300

    Update livroSchema com autor

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,34 +1,44 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import { autorSchema } from "./Autor.js"; // Importando o schema de Autor
 
 // Schema com propriedades do objeto livro e suas validações
 
-const livroSchema = new mongoose.Schema({
+const livroSchema = new mongoose.Schema(
+  {
     id: {
-        type: mongoose.Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
     },
     titulo: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
-        editora: {
-        type: String,
-        required: true,
-        trim: true
+    editora: {
+      type: String,
+      required: true,
+      trim: true,
     },
     preco: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     paginas: {
-        type: Number,
-        required: true
-    }
-}, {
+      type: Number,
+      required: true,
+    },
+    autor: {
+      autorSchema // Referenciando o schema de Autor com o Mongoose
+    },
+  },
+  {
     timestamps: true,
-    versionKey: false
-});
+    versionKey: false,
+  }
+);
 
-const livro = mongoose.model('livros', livroSchema);
+const livro = mongoose.model("livros", livroSchema);
 
 export default livro;
+
+// Se quiser criar um editoraSchema será da mesma forma que o autorSchema
+// e referenciará o schema de Editora no campo editora do livroSchema.
