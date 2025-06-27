a929405414b64387b8d957a1f6d92738f2b31b84
EduardoPSRodrigues <eduardopsrodrigues.dev@gmail.com>
2025-06-27 20:19:37 -0300

    Update autoresRoutes.js

--- src/routes/autoresRoutes.js
+++ src/routes/autoresRoutes.js
@@ -3,10 +3,10 @@ import AutorController from '../controllers/autorController.js';
 
 const router = express.Router();
 
-router.get("/autores", AutorController.listarAutores);
-router.get("/autores/:id", AutorController.listarAutorPorId);
-router.post("/autores", AutorController.cadastrarAutor);
-router.put("/autores/:id", AutorController.atualizarAutor);
-router.delete("/autores/:id", AutorController.deletarAutor);
+router.get('/autores', AutorController.listarAutores);
+router.get('/autores/:id', AutorController.listarAutorPorId);
+router.post('/autores', AutorController.cadastrarAutor);
+router.put('/autores/:id', AutorController.atualizarAutor);
+router.delete('/autores/:id', AutorController.deletarAutor);
 
 export default router;
