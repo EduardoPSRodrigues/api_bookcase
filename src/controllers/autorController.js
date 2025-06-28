c8fd2de86f625da4b40da3de74b5be2f10069f8d
EduardoPSRodrigues <eduardopsrodrigues.dev@gmail.com>
2025-06-27 22:36:59 -0300

    Add middleware no tratamento de erro do autor

--- src/controllers/autorController.js
+++ src/controllers/autorController.js
@@ -1,4 +1,4 @@
-import { Autor } from '../models/Autor.js';
+import { Autor } from "../models/Autor.js";
 
 class AutorController {
   // Listar todos os autores
@@ -14,61 +14,56 @@ class AutorController {
   }
 
   // Listar um autor por ID
-  static async listarAutorPorId(req, res) {
+  static async listarAutorPorId(req, res, next) {
     try {
       const autorEncontrado = await Autor.findById(req.params.id);
-      if (!autorEncontrado)
-        return res.status(404).json({ error: 'Autor não encontrado.' });
-      res.status(200).json(autorEncontrado);
-    } catch (e) {
-      res
-        .status(500)
-        .json({ message: `${e.message} - Erro ao buscar o autor.` });
+
+      if (autorEncontrado !== null) {
+        res.status(200).json(autorEncontrado);
+      } else {
+        return res.status(404).json({ message: "Autor não encontrado." });
+      }
+    } catch (error) {
+      next(error); // Passa o erro para o middleware de tratamento de erros
     }
   }
 
   // Criar um novo autor
-  static async cadastrarAutor(req, res) {
+  static async cadastrarAutor(req, res, next) {
     try {
       const novoAutor = await Autor.create(req.body);
       res
         .status(201)
-        .json({ message: 'Autor criado com sucesso.', autor: novoAutor });
-    } catch (e) {
-      res
-        .status(500)
-        .json({ message: `${e.message} - Erro ao cadastrar um autor.` });
+        .json({ message: "Autor criado com sucesso.", autor: novoAutor });
+    } catch (error) {
+      next(error);
     }
   }
 
   // Atualizar um autor existente
-  static async atualizarAutor(req, res) {
+  static async atualizarAutor(req, res, next) {
     try {
       const autorAtualizado = await Autor.findByIdAndUpdate(
         req.params.id,
         req.body
       );
       if (!autorAtualizado)
-        return res.status(404).json({ error: 'Autor não encontrado.' });
-      res.status(200).json({ message: 'Autor atualizado com sucesso.' });
-    } catch (e) {
-      res
-        .status(500)
-        .json({ message: `${e.message} - Erro ao atualizar o autor.` });
+        return res.status(404).json({ error: "Autor não encontrado." });
+      res.status(200).json({ message: "Autor atualizado com sucesso." });
+    } catch (error) {
+      next(error);
     }
   }
 
   // Deletar um autor
-  static async deletarAutor(req, res) {
+  static async deletarAutor(req, res, next) {
     try {
       const autorDeletado = await Autor.findByIdAndDelete(req.params.id);
       if (!autorDeletado)
-        return res.status(404).json({ error: 'Autor não encontrado.' });
-      res.status(200).json({ message: 'Autor deletado com sucesso.' });
-    } catch (e) {
-      res.status(500).json({
-        message: `${e.message} - Erro no servidor ao deletar o autor.`,
-      });
+        return res.status(404).json({ error: "Autor não encontrado." });
+      res.status(200).json({ message: "Autor deletado com sucesso." });
+    } catch (error) {
+      next(error);
     }
   }
 }
