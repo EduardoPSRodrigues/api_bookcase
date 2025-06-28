5af06c8092f6609c24e636885a53306a02980984
EduardoPSRodrigues <eduardopsrodrigues.dev@gmail.com>
2025-06-27 22:36:26 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,10 +1,10 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 async function conectarBancoDeDados() {
   
   await mongoose.connect(process.env.DB_CONNECTION_STRING);
     
-  console.log('Conexão com o MongoDB Atlas estabelecida com sucesso!');
+  console.log("Conexão com o MongoDB Atlas estabelecida com sucesso!");
   return mongoose.connection;
 
 }
