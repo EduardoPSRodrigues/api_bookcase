11ce2581aef940bf2d1ced2906517f423bba12a1
EduardoPSRodrigues <eduardopsrodrigues.dev@gmail.com>
2025-06-27 22:37:58 -0300

    Trocando aspas simples por dupla

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import LivroController from '../controllers/livroController.js';
+import express from "express";
+import LivroController from "../controllers/livroController.js";
 
 const router = express.Router();
 
-router.get('/livros', LivroController.listarLivros);
-router.get('/livros/busca', LivroController.listarLivrosPorEditora);
-router.get('/livros/:id', LivroController.listarLivroPorId);
-router.post('/livros', LivroController.cadastrarLivro);
-router.put('/livros/:id', LivroController.atualizarLivro);
-router.delete('/livros/:id', LivroController.deletarLivro);
+router.get("/livros", LivroController.listarLivros);
+router.get("/livros/busca", LivroController.listarLivrosPorEditora);
+router.get("/livros/:id", LivroController.listarLivroPorId);
+router.post("/livros", LivroController.cadastrarLivro);
+router.put("/livros/:id", LivroController.atualizarLivro);
+router.delete("/livros/:id", LivroController.deletarLivro);
 
 export default router;
