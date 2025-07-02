05d47c744a888e5e65348786e13bba1740a4e71e
EduardoPSRodrigues <eduardopsrodrigues.dev@gmail.com>
2025-07-02 20:48:40 -0300

    Update manipuladorDeErros.js

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import ErroBase from "../error/ErroBase.js";
+import RequisicaoIncorreta from "../error/RequisicaoIncorreta.js";
+import ErroValidacao from "../error/ErroValidacao.js";
+import NaoEncontrado from "../error/NaoEncontrado.js";
 
 /* Captura erros de validação e outros erros do Mongoose, utilizando sempre o `next` para passar o erro para 
 o middleware. O middleware deve ser configurado no arquivo principal da aplicação (app.js) e utiliza 4 parâmetros */
@@ -7,18 +11,14 @@ function manipuladorDeErros(error, req, res, next) {
   // console.log(error); // Log do erro no console
 
   if (error instanceof mongoose.Error.CastError) {
-    res
-      .status(400)
-      .send({ message: "Um ou mais dados fornecidos estão incorretos." });
+    new RequisicaoIncorreta().enviarResposta(res);
   } else if (error instanceof mongoose.Error.ValidationError) {
-    const mensagensError = Object.values(error.errors)
-      .map(erro => erro.message)
-      .join("; ");
-
-    res.status(400).send({ message: `Os seguintes erros forão encontrados: ${mensagensError}` });
+    new ErroValidacao(error).enviarResposta(res);
+  } else if (error instanceof NaoEncontrado) {
+    error.enviarResposta(res);
   }
   else {
-    res.status(500).json({ message: "Erro interno do servidor." });
+    new ErroBase().enviarResposta(res);
   }
 }
 
