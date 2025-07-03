6864f0f78396a92ef13680af44d6e1bc54ca338f
EduardoPSRodrigues <eduardopsrodrigues.dev@gmail.com>
2025-07-03 14:06:53 -0300

    Update Livro.js

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -16,6 +16,10 @@ const livroSchema = new mongoose.Schema(
     editora: {
       type: String,
       required: [true, "A editora do livro é obrigatória."],
+      enum: {
+        values: ["Editora A", "Editora B", "Editora C"],
+        message: "A editora {VALUE} não é um valor permitido",
+      },
       trim: true,
     },
     preco: {
@@ -25,6 +29,24 @@ const livroSchema = new mongoose.Schema(
     paginas: {
       type: Number,
       required: [true, "O número de páginas do livro é obrigatório."],
+      /* min: [
+        10,
+        "O número de páginas deve ser pelo menos 10. Valor fornecido: {VALUE}",
+      ],
+      max: [
+        5000,
+        "O número de páginas não pode exceder 5.000. Valor fornecido: {VALUE}",
+      ],
+
+      Validação personalizada utilizando Javascript para o número de páginas
+      Pode ser usado o min e max, mas aqui optei usar uma validação personalizada */
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        message: (props) =>
+          `O número de páginas deve estar entre 10 e 5000. Valor fornecido: ${props.value}`,
+      },
     },
     // Referenciando o schema de Autor com o Mongoose
     autor: {
