bea0e25fd90667cdce6e9d2a865ac5bc3473f2d2
EduardoPSRodrigues <eduardopsrodrigues.dev@gmail.com>
2025-07-04 13:43:40 -0300

    Update manipuladorDeErros.js

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import ErroBase from "../error/ErroBase.js";
 import RequisicaoIncorreta from "../error/RequisicaoIncorreta.js";
 import ErroValidacao from "../error/ErroValidacao.js";
-import NaoEncontrado from "../error/NaoEncontrado.js";
 
 /* Captura erros de validação e outros erros do Mongoose, utilizando sempre o `next` para passar o erro para 
 o middleware. O middleware deve ser configurado no arquivo principal da aplicação (app.js) e utiliza 4 parâmetros */
@@ -14,7 +13,7 @@ function manipuladorDeErros(error, req, res, next) {
     new RequisicaoIncorreta().enviarResposta(res);
   } else if (error instanceof mongoose.Error.ValidationError) {
     new ErroValidacao(error).enviarResposta(res);
-  } else if (error instanceof NaoEncontrado) {
+  } else if (error instanceof ErroBase) {
     error.enviarResposta(res);
   }
   else {
